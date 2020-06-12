84c3e7c9003a29c2bee118c92756219a925b5c26
Prabhjot Kaur <61759035+prabdeol89@users.noreply.github.com>
2020-06-11 19:50:35 -0700

    React-Native week 2 Assignment

---
+++ comments.js
@@ -0,0 +1,17 @@
+import * as ActionTypes from './ActionTypes';
+
+export const comments = (state = { errMess: null, comments: [] }, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_COMMENTS:
+            return { ...state, errMess: null, comments: action.payload };
+        case ActionTypes.ADD_COMMENT:
+            let comment = action.payload;
+            comment.id = state.comments.length;
+            return { ...state, comments: [...state.comments, comment] };
+        case ActionTypes.COMMENTS_FAILED:
+            return { ...state, errMess: action.payload };
+
+        default:
+            return state;
+    }
+};
